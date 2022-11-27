80c884545bd35ca252a160334bc3b3f40fa01ea8
IliaBogdanov <bogdanov.ilua@gmail.com>
2022-11-27 14:21:16 +0300

    first commit

---
+++ Lesson 14 path 2UITests/Lesson_14_path_2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Lesson_14_path_2UITestsLaunchTests.swift
+//  Lesson 14 path 2UITests
+//
+//  Created by Илья Богданов on 17.10.2022.
+//
+
+import XCTest
+
+class Lesson_14_path_2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
---
+++ Podfile
@@ -0,0 +1,11 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
+platform :ios, '15.0'
+use_frameworks!
+
+target 'Lesson 14 path 2' do
+pod 'Alamofire', '~> 5.5'
+pod 'SVProgressHUD'
+pod 'RealmSwift'
+
+end
